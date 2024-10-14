49fe637e55b29ec4c247cd8d3f036d146608d566
Anastasia Antipova <antnastya03@gmail.com>
2024-10-14 20:43:43 +0300

    task-6

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from databases import Database 
 import sqlalchemy
 from handlers import users_handler
 from schemas.user import UserCreate, UserAuthorize
 from database import metadata, engine, database
+from utils.token import validate_token_and_role
 
 metadata.create_all(bind=engine)
 
@@ -26,8 +27,16 @@ async def read_users():
 async def create_user(user:UserCreate):
     return await users_handler.create_user(user, database)
 
-#дописать
+
 @app.post("/users/authorize")
 async def authorize_user(user:UserAuthorize):
     return await users_handler.authorize_user(user, database)
     
+@app.get("/with-credentials")
+async def check_credentials(user = Depends(validate_token_and_role([ "approved_user", "admin"]))):
+    return {"msg": "Welcome allowed user"}
+    
+    
+@app.get("/without-credentials")
+async def check_credentials():
+     return {"msg": "Welcome all"}
